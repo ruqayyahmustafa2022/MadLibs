5b1d8d23efa2440e7f226083de64f6e4e7213d87
RUQAYYAH MUSTAFA <ruqayyahmustafa@RUQAYYAHs-MacBook-Pro.local>
2022-02-15 14:58:30 -0600

    Submitting Part 1: get the noun, adjective, and verb inputs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,53 @@
 package com.company;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
      /*   Mad Libs
-
-
-
-        In Terminal navigate to your MadLibs project location
-        In terminal, use git init
-        Copy the Github SSH key for your new Github repo
-        In Terminal Setup the git remote named origin by using the Github SSH key you copied earlier
-        Stage and commit the init changes to your local repo
-        Push changes to Github
         Create 3 size 5 arrays
-        One array should have 5 single word strings, each of which is a noun.
-                One array should have 5 single word strings, each of which is an adjective.
-                One array should have 5 single word strings, each of which is a verb.
-                The Java Util class Random has a nextInt() method that can be used to get a random number from the range of zero to the number passed in as a parameter.
-                Write a small story that randomly gets words from each array to fill in blank points in the story based on the word type.
+        One array should have (1) 5 single word strings, each of which is a noun.
+                (2) One array should have 5 single word strings, each of which is an adjective.
+                (3) One array should have 5 single word strings, each of which is a verb.
+                Note: The Java Util class Random has a nextInt() method that can be used to get a random number from the
+                range of zero to the number passed in as a parameter.
+                (4) Write a small story that randomly gets words from each array to fill in blank points in the story
+                based on the word type.
                 Don’t forget to stage and commit changes in git as you go along
         Bonus: Get the words to fill each array by asking the user to supply them. */
-    }
-}
+        Scanner userInput = new Scanner(System.in);
+        String[] nouns = new String[5];
+        String[] adjectives = new String[5];
+        String[] verbs = new String[5];
+
+        //enter nouns
+       System.out.println("Enter 5 Nouns: ");
+       for(String noun: nouns)
+       {
+           noun = userInput.next();
+           System.out.println("Nouns are: " + noun);
+       }//end nouns
+
+        //enter verbs
+        System.out.println("Enter 5 verbs: ");
+        for(String verb: verbs)
+        {
+            verb = userInput.next();
+            System.out.println("Verbs are: " + verb);
+        }//end for verbs
+
+        //Enter Adjectives
+        System.out.println("Enter 5 adjectives: ");
+        for(String adjective: adjectives)
+        {
+            adjective = userInput.next();
+            System.out.println("Adjectives are: " + adjective);
+        }//end for adjectives
+
+
+
+
+
+    } //end main
+}//end Main
