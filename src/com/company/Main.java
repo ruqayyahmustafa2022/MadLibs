c42e826c61c697b29e9147f516d2ec105848acd5
RUQAYYAH MUSTAFA <ruqayyahmustafa@RUQAYYAHs-MacBook-Pro.local>
2022-02-15 14:32:25 -0600

    Creating new project Matlibs

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+     /*   Mad Libs
+
+
+
+        In Terminal navigate to your MadLibs project location
+        In terminal, use git init
+        Copy the Github SSH key for your new Github repo
+        In Terminal Setup the git remote named origin by using the Github SSH key you copied earlier
+        Stage and commit the init changes to your local repo
+        Push changes to Github
+        Create 3 size 5 arrays
+        One array should have 5 single word strings, each of which is a noun.
+                One array should have 5 single word strings, each of which is an adjective.
+                One array should have 5 single word strings, each of which is a verb.
+                The Java Util class Random has a nextInt() method that can be used to get a random number from the range of zero to the number passed in as a parameter.
+                Write a small story that randomly gets words from each array to fill in blank points in the story based on the word type.
+                Don’t forget to stage and commit changes in git as you go along
+        Bonus: Get the words to fill each array by asking the user to supply them. */
+    }
+}
